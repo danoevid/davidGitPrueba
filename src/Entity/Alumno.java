027bffa0bea466ab041f63fe4daee0ed844d3912
David Eusebio <davideusebio033@gmail.com>
2022-05-12 21:40:35 -0300

    mensaje de confirmacion

---
+++ src/Entity/Alumno.java
@@ -0,0 +1,65 @@
+
+package Entity;
+
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Alumno {
+    
+    @Id
+    private String dni;
+    @Basic
+    private String nombre;
+    private String apellido;
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+
+    public Alumno() {
+    }
+
+    public Alumno(String dni, String nombre, String apellido, Date fechaNacimiento) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    
+}
